d16949f70bd1ac8b7a5771cbc7076dedf64dd6b7
MudassirAlam7 <mudassiralam7@gmail.com>
2024-12-24 00:14:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let sum = 0
+	let price = document.querySelectorAll('.price')
+	for (let t  of price) {
+		sum = sum + parseFloat(t.textContent)||0
+	}
+  let msg = document.createElement('div')
+	msg.innerText = `Totol price : ${sum}`
+	document.body.appendChild(msg)
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
